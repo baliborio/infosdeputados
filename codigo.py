c4394440ded41a587235f7076b37ea1d123cfde5
Bárbara Libório <34222922+baliborio@users.noreply.github.com>
2024-05-21 15:55:28 -0300

    Update codigo.py
    adicionando paginação

--- codigo.py
+++ codigo.py
@@ -1,28 +1,35 @@
 import requests
 import csv
 
-url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=57&ordem=ASC&ordenarPor=nome'
-response = requests.get(url)
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=56&ordem=ASC&ordenarPor=nome'
+pagina = 1  # Começando da primeira página
+deputados = []
 
-if response.status_code == 200:
-    dados = response.json()
-    deputados = []
-    for deputado in dados['dados']:
-        nome = deputado['nome']
-        ide = deputado['id']
-        foto = deputado['urlFoto']
-        partido = deputado['siglaPartido']
-        UF = deputado['siglaUf']
-        dades = [nome, ide, foto, partido, UF]
-        deputados.append(dades)
-        print(dades)
-    
-    # Escrever os dados em um arquivo CSV
-    with open('deputados57.csv', 'w', newline='', encoding='utf-8') as arquivo_csv:
-        writer = csv.writer(arquivo_csv)
-        writer.writerow(['nome', 'id', 'foto', 'partido', 'UF'])  # Escrever o cabeçalho
+while True:
+    response = requests.get(url, params={'pagina': pagina})
+    if response.status_code == 200:
+        dados = response.json()
+        if dados['dados']:  # Verificar se há dados na página
+            for deputado in dados['dados']:
+                nome = deputado['nome']
+                ide = deputado['id']
+                foto = deputado['urlFoto']
+                partido = deputado['siglaPartido']
+                UF = deputado['siglaUf']
+                dades = [nome, ide, foto, partido, UF]
+                deputados.append(dades)
+                print(dades)
+            pagina += 1
+        else:
+            break  # Se não houver mais dados, saia do loop
+    else:
+        print("Erro ao acessar a API da Câmara dos Deputados.")
+        break
 
-        for linha in deputados:
-            writer.writerow(linha)
-else:
-    print("Erro ao acessar a API da Câmara dos Deputados.")
+# Escrever os dados em um arquivo CSV
+with open('deputados562.csv', 'w', newline='', encoding='utf-8') as arquivo_csv:
+    writer = csv.writer(arquivo_csv)
+    writer.writerow(['nome', 'id', 'foto', 'partido', 'UF'])  # Escrever o cabeçalho
+
+    for linha in deputados:
+        writer.writerow(linha)
